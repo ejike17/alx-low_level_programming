045a71681a0d5bb5681dec973bad6977ec7a0f2c
Ejike17 <ejikeanumba17@gmail.com>
2022-12-09 04:57:00 -0800

    Second ALX task on C programming

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,11 +11,11 @@ int main(void)
 
 	for(i = 48; i < 58; i++)
 	{
-		putchar(i);
+	putchar(i);
 		if(i != 57)
 		{
-			putchar(',');
-			putchar(' ');
+		putchar(',');
+		putchar(' ');
 		}
 	}
 	putchar('\n');
