e15268999902cc98c48f60933f863ef052dbf291
Ejike17 <ejikeanumba17@gmail.com>
2022-12-19 14:39:05 -0800

    Pointer in C language

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *
+ *  * * _strcpy - check the code.
+ *
+ *   * * @dest: a pointer to destination of string.
+ *
+*c: a pointer to source string to copy from
+* Return: the pointer to desth
+*/
+
+char *_strcpy(char *dest, char *src)
+{
+	char *c = dest;
+
+	while
+		((*dest++ = *src++));
+	return (c);
+}
